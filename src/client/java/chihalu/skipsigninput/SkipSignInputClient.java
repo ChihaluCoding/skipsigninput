eb040da947547361f7b2d8fbc5e8ce719e6e8620
Chihalu <m.chihiro0314@gmail.com>
2025-10-29 02:45:39 +0900

    first commit

---
+++ src/client/java/chihalu/skipsigninput/SkipSignInputClient.java
@@ -0,0 +1,10 @@
+package chihalu.skipsigninput;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkipSignInputClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/chihalu/skipsigninput/SkipSignInputDataGenerator.java
@@ -0,0 +1,11 @@
+package chihalu.skipsigninput;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkipSignInputDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
